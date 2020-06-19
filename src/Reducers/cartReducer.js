f8f45362d9f449c894f896899ddd835c1df50485
Jake <jakefrisbee@gmail.com>
2020-06-19 07:27:47 -0700

    changed reducer to a switch statment

--- src/Reducers/cartReducer.js
+++ src/Reducers/cartReducer.js
@@ -2,17 +2,18 @@ import { ADD, REMOVE } from './actionTypes';
 const INITIAL_STATE = [];
 
 const cartReducer = (state=INITIAL_STATE, action) => {
-    if (action.type === ADD) {
-        return [...state, action.id];
-    }
+    switch (action.type) {
+        case ADD:
+            return [...state, action.id];
 
-    if (action.type === REMOVE) {
-        const idx = state.findIndex(id => id === action.id);
-        if (idx >= 0) return [...state.slice(0, idx), ...state.slice(idx + 1)];
-        else return state;
+        case REMOVE:
+            const idx = state.findIndex(id => id === action.id);
+            if (idx >= 0) return [...state.slice(0, idx), ...state.slice(idx + 1)];
+            else return state;
 
+        default:
+            return state
     }
-    return state
 }
 
 export default cartReducer;
