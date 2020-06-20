01ab467aac849a6918dae02018044256ee235654
Jake <jakefrisbee@gmail.com>
2020-06-20 13:57:15 -0700

    updated reducer tests to reflect new cart state

--- src/Tests/actionCreators.test.js
+++ src/Tests/actionCreators.test.js
@@ -1,13 +1,18 @@
-import {add, remove } from '../Reducers/actionCreators';
-import { ADD, REMOVE } from '../Reducers/actionTypes';
+import {add, remove, applyDiscount } from '../Reducers/actionCreators';
+import { ADD, REMOVE, APPLY_DISCOUNT } from '../Reducers/actionTypes';
 
 test('will return an object with type:add and id:1', () => {
-    const a = add(1);
-    expect(a).toEqual({type: ADD, id: 1})
+    const a = add(1, 10.50);
+    expect(a).toEqual({type: ADD, id: 1, price: 10.50})
 })
 
 test('will return an object with type:remove and id:1', () => {
     const r = remove(1);
     expect(r).toEqual({type: REMOVE, id: 1})
+})
+
+test('will return an object with type:apply_discount and amount:.1', () => {
+    const apply = applyDiscount(.1)
+    expect(apply).toEqual({type: APPLY_DISCOUNT, amount: .1})
 })
 
--- src/Tests/reducers.test.js
+++ src/Tests/reducers.test.js
@@ -3,16 +3,40 @@ import { ADD, REMOVE } from '../Reducers/actionTypes';
 
 
 test('can add an item to the cart', () => {
-    expect(cartReducer([], {type: ADD, id: 'asd13' })).toEqual([{id: 'asd13', quantity: 1}])
+    expect(cartReducer({items: [],
+        total: 0,
+        discountApplied: false,
+        discountAmount: 0 }, {type: ADD, id: 'asd13', price: 10.50 })).toEqual({items: [{id: 'asd13', quantity: 1, price: 10.50}], 
+                                                                                total: 10.50, 
+                                                                                discountApplied: false, 
+                                                                                discountAmount: 0})
 })
 
 test('can add a duplicate item to the cart', () => {
-    expect(cartReducer([{id: 'asd13', quantity: 1}], {type: ADD, id: 'asd13'})).toEqual([{id: 'asd13', quantity: 2}]);
+    expect(cartReducer({items: [{id: 'asd13', quantity: 1, price: 10.50}], 
+                        total: 10.50, 
+                        discountApplied: false, 
+                        discountAmount: 0}, {type: ADD, id: 'asd13', price: 10.50})).toEqual({items: [{id: 'asd13', quantity: 2, price: 10.50}], 
+                                                                                            total: 21.00, 
+                                                                                            discountApplied: false, 
+                                                                                            discountAmount: 0})
 })
 test('can add a another item to the cart', () => {
-    expect(cartReducer([{id: 'asd13', quantity: 2}], {type: ADD, id: '93ju2'})).toEqual([{id: 'asd13', quantity: 2}, {id: '93ju2' , quantity: 1}]);
+    expect(cartReducer({items: [{id: 'asd13', quantity: 2, price: 10.50}], 
+                        total: 21.00, 
+                        discountApplied: false, 
+                        discountAmount: 0}, {type: ADD, id: '93ju2', price: 2.00})).toEqual({items: [{id: 'asd13', quantity: 2, price: 10.50}, {id: '93ju2', quantity: 1, price: 2.00}], 
+                                                                                            total: 23.00, 
+                                                                                            discountApplied: false, 
+                                                                                            discountAmount: 0});
 })
 
 test('can remove 1 of the duplicate items from the car', () => {
-    expect(cartReducer([{id: 'asd13', quantity: 2}, {id: '93ju2' , quantity: 1}], {type: REMOVE, id: 'asd13'})).toEqual([{id: 'asd13', quantity: 1}, {id: '93ju2' , quantity: 1}]);
+    expect(cartReducer({items: [{id: 'asd13', quantity: 2, price: 10.50}, {id: '93ju2', quantity: 1, price: 2.00}], 
+                        total: 24.00, 
+                        discountApplied: false, 
+                        discountAmount: 0}, {type: REMOVE, id: 'asd13'})).toEqual({items: [{id: 'asd13', quantity: 1, price: 10.50}, {id: '93ju2', quantity: 1, price: 2.00}], 
+                                                                                    total: 12.50, 
+                                                                                    discountApplied: false, 
+                                                                                    discountAmount: 0});
 })
